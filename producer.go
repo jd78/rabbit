6d4114e7a8e217648e9c79064e0a9ff925df276f
Gennaro Del Sorbo <JD78@JD-macbook.local>
2018-01-14 19:01:39 +0100

    producer make it better

--- producer.go
+++ producer.go
@@ -18,6 +18,18 @@ type IProducer interface {
 		contentType ContentType) error
 }
 
+type sendMessage struct {
+	message         interface{}
+	routingKey      string
+	messageID       string
+	messageType     string
+	header          map[string]interface{}
+	contentType     ContentType
+	responseChannel *chan error
+	producer        *producer
+	producerIndex   int
+}
+
 type producer struct {
 	numberOfProducers int
 	channels          []*amqp.Channel
@@ -28,6 +40,7 @@ type producer struct {
 	logLevel          LogLevel
 	confirmPublish    bool
 	confirms          []chan amqp.Confirmation
+	producers         []chan sendMessage
 }
 
 func (r *rabbit) configureProducer(numberOfProducers int, exchangeName string, deliveryMode DeliveryMode,
@@ -40,6 +53,7 @@ func (r *rabbit) configureProducer(numberOfProducers int, exchangeName string, d
 
 	channels := make([]*amqp.Channel, numberOfProducers, numberOfProducers)
 	confirms := make([]chan amqp.Confirmation, numberOfProducers, numberOfProducers)
+	producers := make([]chan sendMessage, numberOfProducers, numberOfProducers)
 	for i := 0; i < numberOfProducers; i++ {
 		channel, err := r.connection.Channel()
 
@@ -68,10 +82,58 @@ func (r *rabbit) configureProducer(numberOfProducers int, exchangeName string, d
 				r.log.warn(fmt.Sprintf("channel flow detected - flow enabled: %t", status))
 			}
 		}()
+
+		producers[i] = make(chan sendMessage, 1)
+		go func(i int) {
+			for {
+				s := <-producers[i]
+				send(&s)
+			}
+		}(i)
 	}
 
 	return &producer{numberOfProducers, channels, 0, exchangeName, deliveryMode, r.log, r.logLevel, confirmPublish,
-		confirms}
+		confirms, producers}
+}
+
+func send(s *sendMessage) {
+	serialized, err := serialize(s.message, s.contentType)
+	checkError(err, "json serializer error", s.producer.log)
+
+	mt := pattern.ResultMatch(s.messageType).
+		WhenValue("", func() interface{} { return s.messageType }).
+		ResultOrDefault(reflect.TypeOf(s.message).String()).(string)
+
+	if s.producer.logLevel >= Debug {
+		s.producer.log.debug(fmt.Sprintf("Sending Message %s: %s", mt, serialized))
+	}
+
+	pErr := s.producer.channels[s.producerIndex].Publish(s.producer.exchangeName, s.routingKey, false, false, amqp.Publishing{
+		Headers:      s.header,
+		ContentType:  string(s.contentType),
+		DeliveryMode: uint8(s.producer.deliveryMode),
+		MessageId:    s.messageID,
+		Timestamp:    time.Now().UTC(),
+		Type:         mt,
+		Body:         serialized,
+	})
+
+	if pErr != nil {
+		*s.responseChannel <- pErr
+		return
+	}
+
+	if s.producer.confirmPublish {
+		if confirmed := <-s.producer.confirms[s.producerIndex]; confirmed.Ack {
+			*s.responseChannel <- nil
+			return
+		}
+
+		*s.responseChannel <- errors.New("unable to publish")
+		return
+	}
+
+	*s.responseChannel <- nil
 }
 
 func (p *producer) getNext() int {
@@ -80,44 +142,26 @@ func (p *producer) getNext() int {
 
 //Send a message.
 //messageType: if empty the message type will be reflected from the message
-func (p *producer) Send(message interface{}, routingKey, messageID string, messageType string, header map[string]interface{}, contentType ContentType) error {
-	i := p.getNext()
-	channel := p.channels[i%p.numberOfProducers]
+func (p *producer) Send(message interface{}, routingKey, messageID, messageType string, header map[string]interface{}, contentType ContentType) error {
+	i := p.getNext() % p.numberOfProducers
+	response := make(chan error, 1)
 
-	serialized, err := serialize(message, contentType)
-	checkError(err, "json serializer error", p.log)
+	//p.log.info(fmt.Sprintf("message id %s, assigned to worker %d", messageID, i))
 
-	mt := pattern.ResultMatch(messageType).
-		WhenValue("", func() interface{} { return messageType }).
-		ResultOrDefault(reflect.TypeOf(message).String()).(string)
-
-	if p.logLevel >= Debug {
-		p.log.debug(fmt.Sprintf("Sending Message %s: %s", mt, serialized))
+	s := sendMessage{
+		producerIndex:   i,
+		contentType:     contentType,
+		header:          header,
+		message:         message,
+		messageID:       messageID,
+		messageType:     messageType,
+		producer:        p,
+		responseChannel: &response,
+		routingKey:      routingKey,
 	}
 
-	pErr := channel.Publish(p.exchangeName, routingKey, false, false, amqp.Publishing{
-		Headers:      header,
-		ContentType:  string(contentType),
-		DeliveryMode: uint8(p.deliveryMode),
-		MessageId:    messageID,
-		Timestamp:    time.Now().UTC(),
-		Type:         mt,
-		Body:         serialized,
-	})
-
-	if pErr != nil {
-		return pErr
-	}
-
-	if p.confirmPublish {
-		if confirmed := <-p.confirms[i]; confirmed.Ack {
-			return nil
-		}
-
-		return errors.New("unable to publish")
-	}
-
-	return nil
+	p.producers[i] <- s
+	return <-response
 }
 
 func serialize(message interface{}, contentType ContentType) ([]byte, error) {
