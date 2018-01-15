1da3aada8f1d4baf5c5e1c4a00b0ce0be23d0936
Gennaro Del Sorbo <JD78@JD-macbook.local>
2018-01-15 23:46:18 +0100

    echange bind

--- topology.go
+++ topology.go
@@ -11,25 +11,31 @@ type topology struct {
 
 func (r *rabbit) topologyConfiguration() *topology {
 	channel, err := r.connection.Channel()
-	checkError(err, "error creating topology channel", r.log)
+	checkError(err, "Error creating topology channel", r.log)
 	return &topology{channel, r.log}
 }
 
 func (t *topology) DeclareExchange(name, kind string, durable, autoDelete, internal bool, args map[string]interface{}) *topology {
 	err := t.channel.ExchangeDeclare(name, kind, durable, autoDelete, internal, false, args)
-	checkError(err, "error creating exchange", t.log)
+	checkError(err, "Error creating exchange", t.log)
 	return t
 }
 
 func (t *topology) DeclareQueue(name string, durable, autoDelete, exclusive bool, args map[string]interface{}) *topology {
 	_, err := t.channel.QueueDeclare(name, durable, autoDelete, exclusive, false, args)
-	checkError(err, "error creating queue", t.log)
+	checkError(err, "Error creating queue", t.log)
 	return t
 }
 
 func (t *topology) BindQueue(name, routingKey, exchangeName string, args map[string]interface{}) *topology {
 	err := t.channel.QueueBind(name, routingKey, exchangeName, false, args)
-	checkError(err, "error creating the queue bind", t.log)
+	checkError(err, "Error creating the queue bind", t.log)
+	return t
+}
+
+func (t *topology) BindExchange(source, destination, routingKey string, args map[string]interface{}) *topology {
+	err := t.channel.ExchangeBind(destination, routingKey, source, false, args)
+	checkError(err, "Error creating the exchange bind", t.log)
 	return t
 }
 
